a4b5f71a558889ce5161c261a7f6fc9b8a51101f
Gil <sgy@tpay.co.kr>
2020-08-07 01:59:56 +0900

    [Fix] 테스트 내 오타 수정

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -1,5 +1,5 @@
 def test_defaults(cookies):
     result = cookies.bake()
-    assert result.proejct.isdir()
+    assert result.project.isdir()
     assert result.exit_code == 0
     assert result.exception is None
