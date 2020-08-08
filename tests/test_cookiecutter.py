044dcea4f87cdf4e8eda15f5a39192a36a4cb2d9
Gil <sgy@tpay.co.kr>
2020-08-08 10:08:14 +0900

    [Fix] Generate Project 테스트가 데이터를 넣었을 때 체크하도록함

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -1,5 +1,19 @@
-def test_defaults(cookies):
-    result = cookies.bake()
-    assert result.project.isdir()
+import pytest
+
+
+@pytest.fixture
+def context():
+    return {
+        "project_name": "My Test Project",
+        "project_slug": "my_test_project",
+        "author": "tech@ashe.kr",
+    }
+
+
+def test_defaults(cookies, context):
+    """Test that project is generated and fully rendered."""
+    result = cookies.bake(extra_context={**context})
     assert result.exit_code == 0
     assert result.exception is None
+    assert result.project.basename == context["project_slug"]
+    assert result.project.isdir()
