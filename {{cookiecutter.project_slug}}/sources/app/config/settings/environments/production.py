06729af51402437c6c02a3d8c0259eabfab47890
Gil <sgy@tpay.co.kr>
2020-08-31 21:02:53 +0900

    [Add] AWS, GCP Storage 관련 설정 추가

--- {{cookiecutter.project_slug}}/sources/app/config/settings/environments/production.py
+++ {{cookiecutter.project_slug}}/sources/app/config/settings/environments/production.py
@@ -1,5 +1,7 @@
-from config.settings.components.common.environ import env  # noqa
-from config.settings.components.security import *  # noqa
+# flake8: noqa
+from config.settings.components.common.app import INSTALLED_APPS
+from config.settings.components.common.environ import env
+from config.settings.components.security import *
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = False
@@ -14,12 +16,66 @@ ALLOWED_HOSTS = [
 # https://docs.djangoproject.com/en/dev/ref/databases/#connecting-to-the-database
 DATABASES = {'default': {'HOST': env('DATABASE_URL')}}
 DATABASES['default']['NAME'] = env('DATABASE_NAME')
-{% if cookiecutter.database == 'postgres' %}  # noqa
+{% if cookiecutter.database == 'postgres' %}
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 DATABASES['default']['PORT'] = env('DATABASE_PORT')
 DATABASES['default']['USER'] = env('DATABASE_USER')
 DATABASES['default']['PASSWORD'] = env('DATABASE_PASSWORD')
-{%- else %}  # noqa
+{% else %}
 DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-{%- endif %}  # noqa
+{% endif %}
 DATABASES['default']['ATOMIC_REQUESTS'] = True
+
+
+{% if cookiecutter.cloud_provider != 'None' -%}
+# STORAGES
+# ------------------------------------------------------------------------------
+# https://django-storages.readthedocs.io/en/latest/#installation
+INSTALLED_APPS += ["storages"]
+{% endif %}
+{% if cookiecutter.cloud_provider == 'AWS' %}
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_QUERYSTRING_AUTH = False
+# DO NOT change these unless you know what you're doing.
+_AWS_EXPIRY = 60 * 60 * 24 * 7
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate"
+}
+#  https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_DEFAULT_ACL = None
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default=None)
+{% elif cookiecutter.cloud_provider == 'GCP' %}
+GS_BUCKET_NAME = env("DJANGO_GCP_STORAGE_BUCKET_NAME")
+GS_DEFAULT_ACL = "publicRead"
+{% endif -%}
+
+{% if cookiecutter.cloud_provider != 'None' %}
+# STATIC
+# ------------------------
+{% if cookiecutter.cloud_provider == 'AWS' %}
+STATICFILES_STORAGE = "utils.storages.StaticRootS3Boto3Storage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+STATIC_URL = f"https://{aws_s3_domain}/static/"
+{% elif cookiecutter.cloud_provider == 'GCP' %}
+STATICFILES_STORAGE = "utils.storages.StaticRootGoogleCloudStorage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.gcloud.GoogleCloudStrategy"
+STATIC_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/static/"
+{% endif %}
+
+# MEDIA
+# ------------------------------------------------------------------------------
+{% if cookiecutter.cloud_provider == 'AWS' %}
+DEFAULT_FILE_STORAGE = "utils.storages.MediaRootS3Boto3Storage"
+MEDIA_URL = f"https://{aws_s3_domain}/media/"
+{% elif cookiecutter.cloud_provider == 'GCP' %}
+DEFAULT_FILE_STORAGE = "utils.storages.MediaRootGoogleCloudStorage"
+MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/media/"
+{% endif %}
